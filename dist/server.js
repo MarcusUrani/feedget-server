89e06ac9f3135f6a3667909c558f4d6f47a0f57d
Marcus Urani <marcus.urani20@gmail.com>
2022-05-10 10:52:41 -0300

    Ajustes para deploy

--- dist/server.js
+++ dist/server.js
@@ -4,20 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const prisma_1 = require("./prisma");
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.post("/feedbacks", async (req, res) => {
-    const { type, comment, screenshot } = req.body;
-    const feedback = await prisma_1.prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot
-        }
-    });
-    return res.status(201).json({ data: feedback });
-});
+app.use(routes_1.routes);
 app.listen(3333, () => {
     console.log("Server aberto na porta 3333");
 });
